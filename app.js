3f6a2ab6901ba4e0ac66e069e25242ef8803fc8d
mcal25 <225284010+mcal25@users.noreply.github.com>
2026-08-06 14:55:18 -0400

    Configure views

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const assetsPath = path.join(__dirname, 'public');
 app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
 
-
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT || 3000;
 
